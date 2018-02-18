912920e07e2430b3c7a970256f3f58aa0a9aeedc
weirdchicken1234 <36597340+weirdchicken1234@users.noreply.github.com>
2018-02-18 18:52:24 +0100

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dpkg-deb -bZlzma Projects/AnchorRaw debs
-dpkg-deb -bZlzma Projects/pollo debs
+dpkg-deb -bZlzma Projects/BioProtect debs
 # dpkg-deb --bZlzma Projects/<projectname> <output folder>
